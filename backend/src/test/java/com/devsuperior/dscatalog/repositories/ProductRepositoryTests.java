61c4cd9053efbc8d8ed85118b1deec8e3a51c9f5
Matheus Diefenthaeler <matheus.axo@gmail.com>
2022-04-21 12:10:54 -0300

    ProductRepository Delete Tests

---
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,45 @@
+package com.devsuperior.dscatalog.repositories;
+
+import com.devsuperior.dscatalog.entities.Product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import java.util.Optional;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+
+  private long existingId;
+  private long nonExistingId;
+
+  @BeforeEach
+  void setup() throws Exception {
+    existingId = 1L;
+    nonExistingId = 1000L;
+  }
+
+  @Autowired private ProductRepository repository;
+
+  @Test
+  public void deleteShouldDeleteObjectWhenIdExists() {
+
+    repository.deleteById(existingId);
+    Optional<Product> result = repository.findById(existingId);
+
+    Assertions.assertFalse(result.isPresent());
+  }
+
+  @Test
+  public void deleteShouldThrowEmptyResultDataAcessExceptionWhenIdDoesNotExist() {
+
+    Assertions.assertThrows(
+        EmptyResultDataAccessException.class,
+        () -> {
+          repository.deleteById(nonExistingId);
+        });
+  }
+}
