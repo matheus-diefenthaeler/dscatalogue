abf343e39ae4951a5ab9ac06607dfbbb73cfcf7a
Matheus Diefenthaeler <matheus.axo@gmail.com>
2022-04-25 18:48:22 -0300

    FindAllPaged tests

--- backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dscatalog.services;
 
+import com.devsuperior.dscatalog.dto.ProductDTO;
 import com.devsuperior.dscatalog.entities.Product;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.exceptions.DataBaseException;
@@ -15,7 +16,9 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -59,6 +62,18 @@ public class ProductServiceTests {
 
     // ********** Save Mock **********
     Mockito.when(repository.save(ArgumentMatchers.any())).thenReturn(product);
+  }
+
+  @Test
+  public void findAllPagedShouldReturnPage(){
+
+    var pageable = PageRequest.of(0,10);
+
+    Page<ProductDTO> result = service.findAllPaged(pageable);
+
+    Assertions.assertNotNull(result);
+    Mockito.verify(repository).findAll(pageable);
+
   }
 
   @Test
